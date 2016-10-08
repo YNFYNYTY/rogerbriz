fc792cca594392fc06ec85be7079b9f007cd5b79
Roger Briz <rogerbriz@Rogers-MacBook-Pro.local>
2016-10-08 18:48:51 +0200

    fisr commit

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+$(function () {
+    $('.tlt').textillate({
+    	loop: true,
+    	minDisplayTime: 1000,
+    });
+});
+$(function () {
+    $('.developer').textillate({
+      loop: true,
+      minDisplayTime: 500,
+    });
+});
+$(function(){
+    $("#interactive_bg").interactive_bg({
+   	strength: 50,
+   	scale: 1,
+   	animationSpeed: "1000ms",
+   	contain: true,
+   	wrapContent: true
+ });
+});
+$(window).resize(function() {
+	  $("#interactive_bg > .ibg-bg").css({
+   width: $(window).outerWidth(),
+   height: $(window).outerHeight()
+  });
+});
+$(document).ready(function() {
+  $('.container_heart').hide().fadeIn(3000);
+});
+$(function(){
+    $(".interactive_bg_works").interactive_bg({
+    strength: 50,
+    scale: 1.2,
+    animationSpeed: "300ms",
+    contain: true,
+    wrapContent: true
+ });
+});
+$(window).resize(function() {
+    $(".interactive_bg_works > .ibg-bg").css({
+   width: $(window).outerWidth(),
+   height: $(window).outerHeight()
+  });
+});
---
+++ js/particlesAppContact.js
@@ -0,0 +1,68 @@
+/* default dom id (particles-js) */
+//particlesJS();
+
+/* config dom id */
+//particlesJS('dom-id');
+
+/* config dom id (optional) + config particles params */
+particlesJS('particles-js', {
+  particles: {
+    color: '#FF9900',
+    color_random: false,
+    shape: 'triangle', // "circle", "edge" or "triangle"
+    opacity: {
+      opacity: 1,
+      anim: {
+        enable: false,
+        speed: 1.5,
+        opacity_min: 0,
+        sync: false
+      }
+    },
+    size: 0,
+    size_random: false,
+    nb: 100,
+    line_linked: {
+      enable_auto: true,
+      distance: 100,
+      color: '#fff',
+      opacity: 1,
+      width: 0.5,
+      condensed_mode: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600
+      }
+    },
+    anim: {
+      enable: true,
+      speed: 1
+    }
+  },
+  interactivity: {
+    enable: false,
+    mouse: {
+      distance: 300
+    },
+    detect_on: 'canvas', // "canvas" or "window"
+    mode: 'grab', // "grab" or false
+    line_linked: {
+      opacity: .5
+    },
+    events: {
+      onclick: {
+        enable: false,
+        mode: 'push', // "push" or "remove"
+        nb: 4
+      },
+      onresize: {
+        enable: true,
+        mode: 'out', // "out" or "bounce"
+        density_auto: false,
+        density_area: 800 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
+      }
+    }
+  },
+  /* Retina Display Support */
+  retina_detect: true
+});
